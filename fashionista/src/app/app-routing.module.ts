5122e3a3579ad54258fbe97db7791106b96351c0
salwaAlhajjaji <salwa.alhajjaji@hotmail.com>
2022-04-11 07:08:08 +0300

    DONE :)

--- fashionista/src/app/app-routing.module.ts
+++ fashionista/src/app/app-routing.module.ts
@@ -20,94 +20,40 @@ import { RouterModule, Routes } from '@angular/router';
 import { NewUserComponent } from './admin/new-user/new-user.component';
 import { AuthGuard } from './guard/auth.guard';
 
-// const routes: Routes = [
-//     // main page routing
-//   { path: '', component: HomeComponent },
-//   {path:'**', component: HomeComponent},
-//   { path: 'about-us', component: AboutUsComponent },
-//   // products routing
-//   { path: 'signin', component: SigninComponent },
-//   { path: 'products', component: ProductsComponent }, 
-//   {path: 'product-details/:id', component: PrdouctDetailsComponent},
-//   {path: 'wish-list', component: WishListComponent},
-//   {path: 'cart', component: CartComponent},
-//   {path: 'checkout', component: CheckoutComponent},
-//    // admin routing
-//   { path: 'admin', component: HomeAdminComponent,pathMatch:'full', children :[
-//     // { path: '', component: HomeAdminComponent },
-//     // { path: './sigin', component: SigninAdminComponent },
-//     { path: 'products-list', component: ProductsListComponent, children:[
-//         { path: 'new-product', component: NewProductComponent },
-//         { path: 'edit-product/:id', component: EditProductComponent },
-//       ] 
-//     },
-//     { path: 'users-list', component: UsersAdminComponent,children:[
-//         { path: 'new-user', component: NewUserComponent },
-//         { path: 'edit-user/:id', component: EditUserComponent },
-//       ]
-//      },
-//   ] },
-
-//   // {path: 'admin', component: HomeAdminComponent},
-//   // { path: 'sigin-admin', component: SigninAdminComponent },
-//   // { path: 'products-list', component: ProductsListComponent },
-//   // { path: 'users-list', component: UsersAdminComponent },
-//   // { path: 'users-admin', component: UsersAdminComponent },
-//   // { path: 'new-product', component: NewProductComponent },
-//   // { path: 'edit-product/:id', component: EditProductComponent },
-//   // { path: 'new-user', component: NewUserComponent },
-//   // { path: 'edit-user/:id', component: EditUserComponent },
-//   // { path: 'admin', component: HomeAdminComponent },
-//   { path: '', canActivate: [ AuthGuard ], component: HomeComponent}
-
-// ];
-
 const routes: Routes = [
   // main page routing
-{ path: '', component: HomeComponent },
-// error 404
-// { path: '**', component: HomeComponent },
-{ path: 'about-us', component: AboutUsComponent },
-// products routing
-{ path: 'signin', component: SigninComponent },
-{ path: 'products', component: ProductsComponent }, 
-{path: 'product-details/:id', component: PrdouctDetailsComponent},
-{ path: 'product-details', redirectTo: '/products', pathMatch: 'full' },
-{path: 'wish-list', component: WishListComponent},
-{path: 'cart', component: CartComponent},
-{path: 'checkout', component: CheckoutComponent},
- // admin routing
-//  { path: 'admin',  component: HomeAdminComponent, children :[
+  { path: '', component: HomeComponent },
+  // error 404
+  { path: 'about-us', component: AboutUsComponent },
+  // products routing
+  { path: 'signin', component: SigninComponent },
+  { path: 'products', component: ProductsComponent },
+  { path: 'product-details/:id', component: PrdouctDetailsComponent },
+  { path: 'product-details', redirectTo: '/products', pathMatch: 'full' },
+  { path: 'wish-list', component: WishListComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'checkout', component: CheckoutComponent },
+  // admin routing
   { path: 'signin-admin', component: SigninAdminComponent },
-
-{ path: 'admin', canActivate: [ AuthGuard ], component: HomeAdminComponent, children :[
-  // { path: '', component: HomeAdminComponent },
-  { path: 'products',  component: ProductsHomeComponent, children:[
-      {path: '', component: ProductsListComponent},
-      { path: 'new-product',  component: NewProductComponent },
-      { path: 'edit-product/:id', component: EditProductComponent },
-    ] 
-  },
-  { path: 'users', component: UsersHomeComponent, children:[
-      {path: '', component: UsersAdminComponent},
-      { path: 'new-user', component: NewUserComponent },
-      { path: 'edit-user/:id', component: EditUserComponent },
+  {
+    path: 'admin', canActivate: [AuthGuard], component: HomeAdminComponent, children: [
+      {
+        path: 'products', component: ProductsHomeComponent, children: [
+          { path: '', component: ProductsListComponent },
+          { path: 'new-product', component: NewProductComponent },
+          { path: 'edit-product/:id', component: EditProductComponent },
+        ]
+      },
+      {
+        path: 'users', component: UsersHomeComponent, children: [
+          { path: '', component: UsersAdminComponent },
+          { path: 'new-user', component: NewUserComponent },
+          { path: 'edit-user/:id', component: EditUserComponent },
+        ]
+      },
     ]
-   },
-] },
-
-// {path: 'admin', component: HomeAdminComponent},
-// { path: 'sigin-admin', component: SigninAdminComponent },
-// { path: 'products-list', component: ProductsListComponent },
-// { path: 'users-list', component: UsersAdminComponent },
-// { path: 'users-admin', component: UsersAdminComponent },
-// { path: 'new-product', component: NewProductComponent },
-// { path: 'edit-product/:id', component: EditProductComponent },
-// { path: 'new-user', component: NewUserComponent },
-// { path: 'edit-user/:id', component: EditUserComponent },
-// { path: 'admin', component: HomeAdminComponent },
-// { path: '', canActivate: [ AuthGuard ], component: HomeComponent}
-
+  },
+    { path: '**',  redirectTo: '/' },
 
 
 ];
